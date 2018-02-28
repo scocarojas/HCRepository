2f18d451e938abf9ead26710404fe061e896fb95
Bencomo <development@benfont.com>
2018-02-28 13:43:41 +0000

    env.py created

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ.setdefault("DATABASE_URL", "postgres://sehfdukznkcnzb:77264a00a143a43a50ef5a4f294876dd924e5fdeca477628cbbee89932a4c0da@ec2-79-125-110-209.eu-west-1.compute.amazonaws.com:5432/dcl2n1b5cfc1g3")
